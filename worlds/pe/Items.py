3e9584e25ce5a2d98cb2a19678cdb43d3e36cc20
chrisaegrimm <80452739+chrisaegrimm@users.noreply.github.com>
2024-11-30 15:41:58 -0500

    Update Items.py

--- worlds/pe/Items.py
+++ worlds/pe/Items.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Dict
+from typing import NamedTuple, Dict, Optional
 from BaseClasses import Item, ItemClassification
 
 
@@ -6,7 +6,7 @@ class PEItem(Item):
     game = "Parasite Eve"
 
 class PEItemData(NamedTuple):
-    code: int
+    code: typing.Optional[int]
     category: str
     classification: any
 
@@ -295,4 +295,4 @@ item_data_table: Dict[str, PEItemData] = {
     "Impact Trap":      PEItemData(624275, "Trap",        ItemClassification.trap),
 }
 
-item_table = {name: data.code for name, data in item_data_table.items()}
+item_table = {name: data.code for name, data in item_data_table.items() if data.code is not None}
