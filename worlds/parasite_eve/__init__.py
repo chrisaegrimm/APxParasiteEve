b3b82e319ebe8d36e36d03b508053f668712d776
chrisaegrimm <chrisaegrimm@gmail.com>
2025-03-08 01:21:54 -0500

    PyCharm Test

--- worlds/parasite_eve/__init__.py
+++ worlds/parasite_eve/__init__.py
@@ -7,7 +7,6 @@ from worlds.generic.Rules import set_rule
 from .Items import PEItem, item_data_table, item_table
 from .Locations import PELocation, location_data_table, location_table
 from .Options import PEOptions, pe_option_groups
-from . import Logic
 
 
 class PEWebWorld(WebWorld):
