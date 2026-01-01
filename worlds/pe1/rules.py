ed5908b8014c006838c00c6b294304cfb6db553d
chrisaegrimm <chrisaegrimm@gmail.com>
2025-12-31 21:33:22 -0500

    APWORLD REWORK: Basic Logic Gates & Area Access Item Renaming

--- worlds/pe1/rules.py
+++ worlds/pe1/rules.py
@@ -88,7 +88,71 @@ def set_all_entrance_rules(world: PE1World) -> None:
     cb_k7_to_cb_spire = world.get_entrance("CB:K7 to CB:Boss+3C")
 
 
-# Coming back after items are defined, then will create region locks...
+    set_rule(manhattan_to_ch_norq, lambda state: state.has("Area: Carnegie Hall", world.player))
+    set_rule(manhattan_to_pd_norq, lambda state: state.has("Area: N.Y.P.D. #17", world.player))
+    set_rule(manhattan_to_cp_norq, lambda state: state.has("Area: Central Park", world.player))
+    set_rule(manhattan_to_sh_norq, lambda state: state.has("Area: Soho", world.player))
+    set_rule(manhattan_to_hs_norq, lambda state: state.has("Area: Hospital", world.player))
+    set_rule(manhattan_to_ct_norq_or_cmbt_gk, lambda state: state.has("Area: Chinatown", world.player))
+    set_rule(manhattan_to_sw_cmbt_pk_or_gk, lambda state: state.has_all(("Area: Subway", "Gate Key"), world.player))
+    set_rule(manhattan_to_wh_norq, lambda state: state.has("Area: Warehouse", world.player))
+    set_rule(manhattan_to_mu_norq, lambda state: state.has("Area: Museum", world.player))
+    set_rule(manhattan_to_cb_k1, lambda state: state.has_all(("Area: Chrysler BLDG.", "Chrysler Key 1"), world.player))
+
+    set_rule(ch_norq_to_ch_cmbt, lambda state: state.has("Junk", world.player))
+    set_rule(ch_cmbt_to_ch_cmbt_tk, lambda state: state.has("Theater Key", world.player))
+    set_rule(ch_cmbt_to_ch_cmbt_rk, lambda state: state.has("Rehearse Key", world.player))
+    set_rule(ch_cmbt_rk_to_ch_boss_rk, lambda state: state.has("Junk", world.player))
+
+    set_rule(pd_norq_to_pd_lk, lambda state: state.has("Locker Key", world.player))
+    set_rule(pd_norq_to_pd_sk, lambda state: state.has("Storage Key", world.player))
+    set_rule(pd_norq_to_pd_sj, lambda state: state.has("Super Junk", world.player))
+    set_rule(pd_sj_to_pd_cmbt_sj, lambda state: state.has("Junk", world.player))
+    set_rule(pd_cmbt_sj_to_pd_boss_sj, lambda state: state.has("Junk", world.player))
+    set_rule(pd_boss_sj_to_pd_boss_sj_junk, lambda state: state.has("Junk", world.player))
+    set_rule(pd_boss_sj_to_pd_boss_sj_12rt, lambda state: state.has("Junk", world.player))
+    set_rule(pd_boss_sj_12rt_to_pd_boss_sj_14rt, lambda state: state.has("Junk", world.player))
+
+    set_rule(cp_norq_to_cp_cmbt, lambda state: state.has("Junk", world.player))
+    set_rule(cp_cmbt_to_cp_cmbt_zk, lambda state: state.has("Zoo Key", world.player))
+    set_rule(cp_cmbt_zk_to_cp_boss_zk, lambda state: state.has("Junk", world.player))
+
+    set_rule(sh_norq_to_sh_wm, lambda state: state.has("Weapon Memo", world.player))
+
+    set_rule(hs_norq_to_hs_gc, lambda state: state.has("Green Cardkey", world.player))
+    set_rule(hs_norq_to_hs_bc, lambda state: state.has("Blue Cardkey", world.player))
+    set_rule(hs_norq_to_hs_cmbt, lambda state: state.has("Junk", world.player))
+    set_rule(hs_cmbt_to_hs_cmbt_ak, lambda state: state.has("Autopsy Key", world.player))
+    set_rule(hs_gc_to_hs_cmbt_gc, lambda state: state.has("Junk", world.player))
+    set_rule(hs_bc_to_hs_cmbt_bc, lambda state: state.has("Junk", world.player))
+    set_rule(hs_cmbt_bc_to_hs_cmbt_bc_3f, lambda state: state.has_all(("Fuse 1", "Fuse 2", "Fuse 3"), world.player))
+    set_rule(hs_cmbt_bc_3f_to_hs_boss_bc_3f_ek, lambda state: state.has("Elevator Key", world.player))
+
+    set_rule(ct_norq_or_cmbt_gk_to_ct_cmbt_or_cmbt_gk, lambda state: state.has("Junk", world.player))
+    set_rule(ct_cmbt_or_cmbt_gk_to_ct_boss_or_boss_gk, lambda state: state.has("Junk", world.player))
+    set_rule(ct_cmbt_or_cmbt_gk_to_ct_cmbt_or_gk, lambda state: state.has("Pump Key", world.player))
+    set_rule(ct_cmbt_or_gk_to_sw_cmbt_pk_or_gk, lambda state: state.has_all(("Area: Chinatown", "Area: Subway"), world.player))
+
+    set_rule(sw_cmbt_pk_or_gk_to_sw_cmbt_pk_or_cmbt_gk, lambda state: state.has("Junk", world.player))
+    set_rule(sw_cmbt_pk_or_cmbt_gk_to_sw_boss_pk_or_boss_gk, lambda state: state.has("Junk", world.player))
+
+    set_rule(wh_norq_to_wh_cmbt, lambda state: state.has("Junk", world.player))
+    set_rule(wh_cmbt_to_wh_cmbt_wk, lambda state: state.has("Warehouse Key", world.player))
+    set_rule(wh_cmbt_wk_to_wh_boss_wk, lambda state: state.has("Junk", world.player))
+
+    set_rule(mu_norq_to_mu_cmbt, lambda state: state.has("Junk", world.player))
+    set_rule(mu_cmbt_to_mu_cmbt_kk, lambda state: state.has("Klamp Key", world.player))
+    set_rule(mu_cmbt_kk_to_mu_boss_kk, lambda state: state.has("Junk", world.player))
+    set_rule(mu_boss_kk_to_liberty, lambda state: state.has_all(("Narita", "Mayoke", "Hamaya", "M8000"), world.player))
+    set_rule(liberty_to_cruiser, lambda state: state.has("DNA Bullets", world.player))
+
+    set_rule(cb_k1_to_cb_k2, lambda state: state.has_all(("Chrysler Key 2", "M8000"), world.player))
+    set_rule(cb_k2_to_cb_k3, lambda state: state.has("Chrysler Key 3", world.player))
+    set_rule(cb_k3_to_cb_k4, lambda state: state.has("Chrysler Key 4", world.player))
+    set_rule(cb_k4_to_cb_k5, lambda state: state.has("Chrysler Key 5", world.player))
+    set_rule(cb_k5_to_cb_k6, lambda state: state.has("Chrysler Key 6", world.player))
+    set_rule(cb_k6_to_cb_k7, lambda state: state.has("Chrysler Key 7", world.player))
+    set_rule(cb_k7_to_cb_spire, lambda state: state.has_all(("Narita", "Mayoke", "Hamaya", "MP5A5"), world.player))
 
 def set_completion_condition(world: PE1World) -> None:
 
